24239e77bf080fc64a65aa21d9dddaf8abf03161
StarCloud <115127996+StarNGK@users.noreply.github.com>
2025-12-15 10:27:12 +0800

    Update task.go

--- node/task.go
+++ node/task.go
@@ -4,9 +4,9 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	panel "github.com/wyx2685/v2node/api/v2board"
-	"github.com/wyx2685/v2node/common/task"
-	vCore "github.com/wyx2685/v2node/core"
+	panel "github.com/starngk/v2node/api/v2board"
+	"github.com/starngk/v2node/common/task"
+	vCore "github.com/starngk/v2node/core"
 )
 
 func (c *Controller) startTasks(node *panel.NodeInfo) {
